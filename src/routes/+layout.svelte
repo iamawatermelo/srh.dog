a36515b85902e4b93dfe679f2bd56013624fee77
iamawatermelo <144068104+iamawatermelo@users.noreply.github.com>
2026-01-22 17:57:51 +0000

    favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
 	import '$lib/app.css';
+	import faviconUrl from '$lib/favicon.svg?url';
 
 	const { children } = $props();
 </script>
+
+<svelte:head>
+    <link rel="icon" href={faviconUrl} sizes="any" type="image/svg+xml">
+</svelte:head>
 
 {@render children()}
